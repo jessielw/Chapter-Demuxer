5dbf4696a7f5917ea85a6d58af091fe2e12f2251
jlw4049 <jlw_4049@hotmail.com>
2022-01-28 16:09:55 -0500

    Output button is now functional

--- Chapter-Extracting-Tool.py
+++ Chapter-Extracting-Tool.py
@@ -1,35 +1,37 @@
-#Import the required libraries
+# Import the required libraries
 from tkinter import filedialog, StringVar, ttk, messagebox, PhotoImage, Menu, LabelFrame, E, N, S, W, Label, \
     Entry, DISABLED, NORMAL, END, Frame, Spinbox, CENTER, Checkbutton, HORIZONTAL, Toplevel, SUNKEN, OptionMenu, Button
 
 from TkinterDnD2 import *
 import subprocess, pathlib
 
-
-chap_extract_win = TkinterDnD.Tk()
-chap_extract_win .configure(background="#434547")  # Sets gui background color
+chap_extract_win = TkinterDnD.Tk()  # Main loop with DnD.Tk() module (for drag and drop)
+chap_extract_win.title('Chapter-Extracting-Tool 1.0')  # Sets the version of the program
+chap_extract_win.configure(background="#434547")  # Sets gui background color
 window_height = 276  # Gui window height
 window_width = 446  # Gui window width
-screen_width = chap_extract_win .winfo_screenwidth()  # down
-screen_height = chap_extract_win .winfo_screenheight()  # down
+screen_width = chap_extract_win.winfo_screenwidth()  # down
+screen_height = chap_extract_win.winfo_screenheight()  # down
 x_coordinate = int((screen_width / 2) - (window_width / 2))  # down
 y_coordinate = int((screen_height / 2) - (window_height / 2))  # down
-chap_extract_win .geometry(f'{window_width}x{window_height}+{x_coordinate}+{y_coordinate}')  # does the math for center open
+chap_extract_win.geometry(f'{window_width}x{window_height}+{x_coordinate}+{y_coordinate}')  # opens window center
 
 chap_extract_win.rowconfigure(3, weight=1)
 chap_extract_win.grid_columnconfigure(2, weight=1)
 
-audio_title_entrybox_label = Label(chap_extract_win, text='Chapter Extractor Tool (MKV and MP4)', anchor=CENTER, background='#434547',
+audio_title_entrybox_label = Label(chap_extract_win, text='Chapter Extractor Tool (MKV and MP4)', anchor=CENTER,
+                                   background='#434547',
                                    foreground='green')
 audio_title_entrybox_label.grid(row=0, column=2, columnspan=1, padx=20, pady=(5, 0), sticky=W + E)
 audio_title_entrybox_label.config(font=('Arial Black', 11))
 
-chapter_extract = LabelFrame(chap_extract_win , text=' Chapter Extraction ')
+chapter_extract = LabelFrame(chap_extract_win, text=' Chapter Extraction ')
 chapter_extract.grid(row=1, columnspan=3, sticky=E + W + N + S, padx=20, pady=(5, 0))
 chapter_extract.configure(fg="white", bg="#434547", bd=4)
 
 chapter_extract.grid_columnconfigure(0, weight=1)
 chapter_extract.grid_rowconfigure(0, weight=1)
+
 
 def input_button_commands():  # Open file block of code (non drag and drop)
     global VideoInput, autosavefilename, autofilesave_dir_path, VideoInputQuoted, output, detect_video_fps, \
@@ -153,16 +155,30 @@ chap_input_entry.drop_target_register(DND_FILES)
 chap_input_entry.dnd_bind('<<Drop>>', video_drop_input)
 
 
-chap_output_button = Button(chapter_extract, text='Output', command=input_button_commands, foreground='white',
-                           background='#23272A', borderwidth='3', activebackground='grey', width=15)
+def output_button_commands():
+    global output, output_quoted
+    output_window = filedialog.asksaveasfilename(defaultextension=".txt", initialdir=autofilesave_dir_path,
+                                                 title="Select a Save Location", initialfile=autosavefilename,
+                                                 filetypes=[("ogg.txt", "*.txt")])
+
+    if output_window:
+        chap_output_entry.configure(state=NORMAL)
+        chap_output_entry.delete(0, END)
+        output_quoted = '"' + str(pathlib.Path(output_window)) + '"'
+        output = output_window
+        chap_output_entry.insert(0, output)
+        chap_output_entry.configure(state=DISABLED)
+
+
+chap_output_button = Button(chapter_extract, text='Output', command=output_button_commands, foreground='white',
+                            background='#23272A', borderwidth='3', activebackground='grey', width=15)
 chap_output_button.grid(row=1, column=0, columnspan=1, padx=(10, 5), pady=(40, 5), sticky=W + E)
 chap_output_entry = Entry(chapter_extract, borderwidth=4, background='#CACACA', state=DISABLED, width=30)
 chap_output_entry.grid(row=1, column=1, columnspan=2, padx=(5, 10), pady=(40, 5), sticky=W + E)
 
 extract_button = Button(chap_extract_win, text='Extract', command=input_button_commands, foreground='white',
-                           background='#23272A', borderwidth='3', activebackground='grey', width=15, state=DISABLED)
+                        background='#23272A', borderwidth='3', activebackground='grey', width=15, state=DISABLED)
 extract_button.grid(row=2, column=2, columnspan=1, padx=(20, 20), pady=(40, 10), sticky=W + E)
-
 
 # mp4box = r"C:\Users\jlw_4\Desktop\mp4box.exe"
 #
@@ -181,9 +197,7 @@ extract_button.grid(row=2, column=2, columnspan=1, padx=(20, 20), pady=(40, 10),
 # print(test)
 
 
-
 # mkv extract command = C:\Users\jlw_4\Desktop\mkvtoolnix\mkvextract.exe "\\Jlwserver\h\Futurama\Futurama.S01.NTSC.DVD.DD.2.0.MPEG-2.REMUX-RPG\Futurama - 1x01 - Space Pilot 3000.mkv" chapters -s C:\Users\jlw_4\Desktop\mkvchapters.txt
 
 
 chap_extract_win.mainloop()
-
