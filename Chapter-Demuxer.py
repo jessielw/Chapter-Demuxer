0d4d8e2cf54bb15bac6a23cd0c8f2cf328e76a11
jlw4049 <jlw_4049@hotmail.com>
2022-01-28 21:39:21 -0500

    Final check
    * Combed over the code, did some quick comments
    * Removed un-needed imports and added a needed import
    * Stable

--- Chapter-Demuxer.py
+++ Chapter-Demuxer.py
@@ -1,19 +1,26 @@
-standalone = True
+# Code needed for Standalone Release-----------------------------------------------------------------------------------
+standalone = True  # Set to false if paired with a main GUI
+
+
+# ------------------------------------------------------------------------------------------ Code needed for Standalone
 
 def launch_chapter_demuxer():
-    # Import the required libraries
-    from tkinter import filedialog, StringVar, ttk, messagebox, PhotoImage, Menu, LabelFrame, E, N, S, W, Label, \
-        Entry, DISABLED, NORMAL, END, Frame, Spinbox, CENTER, Checkbutton, HORIZONTAL, Toplevel, SUNKEN, OptionMenu, \
-        Button
+    # Imports ---------------------------------------------------------------------------------------------------------
+    from tkinter import filedialog, StringVar, messagebox, LabelFrame, E, N, S, W, Label, Entry, DISABLED, NORMAL, \
+        END, Toplevel, SUNKEN, Button
     import subprocess, pathlib
     from pymediainfo import MediaInfo
+    from configparser import ConfigParser
 
     global chap_extract_win
 
+    # --------------------------------------------------------------------------------------------------------- Imports
+
+    # Main Gui & Windows ----------------------------------------------------------------------------------------------
     try:  # Checks rather or not the youtube-dl-gui window is already open
         if chap_extract_win is not None or Toplevel.winfo_exists(chap_extract_win):
-            chap_extract_win.lift()  # If youtube-dl-gui window exists then bring to top of all other windows
-    except(Exception,):
+            chap_extract_win.lift()  # If chapter window exists then bring to top of all other windows
+    except(Exception,):  # If it does not exist, create it...
         if standalone:
             from TkinterDnD2 import TkinterDnD, DND_FILES
             chap_extract_win = TkinterDnD.Tk()  # Main loop with DnD.Tk() module (for drag and drop)
@@ -27,7 +34,7 @@ def launch_chapter_demuxer():
         screen_height = chap_extract_win.winfo_screenheight()  # down
         x_coordinate = int((screen_width / 2) - (window_width / 2))  # down
         y_coordinate = int((screen_height / 2) - (window_height / 2))  # down
-        chap_extract_win.geometry(f'{window_width}x{window_height}+{x_coordinate}+{y_coordinate}')  # opens window center
+        chap_extract_win.geometry(f'{window_width}x{window_height}+{x_coordinate}+{y_coordinate}')  # opens gui center
 
         chap_extract_win.rowconfigure(3, weight=1)
         chap_extract_win.grid_columnconfigure(2, weight=1)
@@ -42,8 +49,7 @@ def launch_chapter_demuxer():
             mp4box = config['mp4box_path']['path']
             mkvextract = config['mkvextract_path']['path']
 
-
-        # Hover over button theme ---------------------------------------
+        # Hover over button theme -------------------------------------------------------------------------------------
         class HoverButton(Button):
             def __init__(self, master, **kw):
                 Button.__init__(self, master=master, **kw)
@@ -57,8 +63,9 @@ def launch_chapter_demuxer():
             def on_leave(self, e):
                 self['background'] = self.defaultBackground
 
+        # ------------------------------------------------------------------------------------- Hover over button theme
 
-        # --------------------------------------- Hover over button theme
+        # chapter frame -----------------------------------------------------------------------------------------------
         chapter_extract = LabelFrame(chap_extract_win, text=' Chapter Extraction ')
         chapter_extract.grid(row=0, columnspan=3, sticky=E + W + N + S, padx=20, pady=(5, 0))
         chapter_extract.configure(fg="white", bg="#434547", bd=4)
@@ -66,64 +73,65 @@ def launch_chapter_demuxer():
         chapter_extract.grid_columnconfigure(0, weight=1)
         chapter_extract.grid_rowconfigure(0, weight=1)
 
+        # ----------------------------------------------------------------------------------------------- chapter frame
 
+        # Input Functions Button --------------------------------------------------------------------------------------
         def input_button_commands():  # Open file block of code (non drag and drop)
             global VideoInput, autosavefilename, autofilesave_dir_path, VideoInputQuoted, output, detect_video_fps, \
                 fps_entry, output_quoted, extension_type
             source_input = filedialog.askopenfilename(initialdir="/", title="Select A File",
                                                       filetypes=[("Supported Formats", ('.mp4', '.mkv'))])
-            chap_input_entry.configure(state=NORMAL)
-            chap_input_entry.delete(0, END)
-            chapter_source_input = source_input
-            media_info = MediaInfo.parse(pathlib.Path(chapter_source_input))
+            chap_input_entry.configure(state=NORMAL)  # Enable chapter input entry
+            chap_input_entry.delete(0, END)  # Clears chapter input entry if there is anything there
+            chapter_source_input = source_input  # Sets variable from source_input (open file gui)
+            media_info = MediaInfo.parse(pathlib.Path(chapter_source_input))  # Uses media info to parse input
             for track in media_info.tracks:
                 if track.track_type == 'General':
-                    detect_chapters = track.count_of_menu_streams
-            if detect_chapters is None:
+                    detect_chapters = track.count_of_menu_streams  # Checks to see if any chapter tracks exist
+            if detect_chapters is None:  # If there is no chapters in input file, show an error message
                 messagebox.showerror(title='Error', message='Input has no chapters')
-            elif detect_chapters is not None:
-                if chapter_source_input.endswith(('.mp4', '.mkv')):
-                    if chapter_source_input.endswith('.mp4'):
-                        extension_type = '.mp4'
-                    if chapter_source_input.endswith('.mkv'):
-                        extension_type = '.mkv'
+            elif detect_chapters is not None:  # If there is chapters continue
+                if chapter_source_input.endswith(('.mp4', '.mkv')):  # If file is either mp4 or mkv
+                    if chapter_source_input.endswith('.mp4'):  #
+                        extension_type = '.mp4'  #
+                    if chapter_source_input.endswith('.mkv'):  #
+                        extension_type = '.mkv'  # The above code sets the input extension to a variable
                     autofilesave_file_path = pathlib.Path(chapter_source_input)  # Command to get file input location
                     autofilesave_dir_path = autofilesave_file_path.parents[0]  # Final command to get only the directory
-                    VideoInputQuoted = '"' + str(pathlib.Path(chapter_source_input)) + '"'
-                    chap_input_entry.insert(0, pathlib.Path(str(chapter_source_input)))
-                    chap_input_entry.configure(state=DISABLED)
-                    filename = pathlib.Path(chapter_source_input)
-                    chapt_input_filename = filename.with_suffix('')
-                    autosavefilename = str(chapt_input_filename.name) + '.Extracted_Chapters'
+                    VideoInputQuoted = '"' + str(pathlib.Path(chapter_source_input)) + '"'  # Quoted input for CLI
+                    chap_input_entry.insert(0, pathlib.Path(str(chapter_source_input)))  # Insert path into entry box
+                    chap_input_entry.configure(state=DISABLED)  # Disable entry box again
+                    filename = pathlib.Path(chapter_source_input)  # New variable to manipulate
+                    chapt_input_filename = filename.with_suffix('')  # filename with suffix
+                    autosavefilename = str(chapt_input_filename.name) + '.Extracted_Chapters'  # get only filename
                     autosave_file_dir = pathlib.Path(str(f'{autofilesave_dir_path}\\') + str(autosavefilename + '.txt'))
-                    output = str(autosave_file_dir)
-                    output_quoted = '"' + output + '"'
+                    output = str(autosave_file_dir)  # both lines give us an automatic save path
+                    output_quoted = '"' + output + '"'  # quoted output
                     chap_output_entry.configure(state=NORMAL)
                     chap_output_entry.delete(0, END)
                     chap_output_entry.insert(0, str(autosave_file_dir))
                     chap_output_entry.configure(state=DISABLED)
                     extract_button.configure(state=NORMAL)
-                    status_label.configure(text='Select Extract')
+                    status_label.configure(text='Select Extract')  # Change bottom status label
                 else:
                     messagebox.showinfo(title='Input Not Supported', message='Try again with a supported file!\n\n' +
                                                                              'Unsupported file extension "' +
-                                                                             str(pathlib.Path(chapter_source_input).suffix) +
-                                                                             '"')
+                                                                             str(pathlib.Path(
+                                                                                 chapter_source_input).suffix) + '"')
                     extract_button.configure(state=DISABLED)
 
+        # -------------------------------------------------------------------------------------- Input Functions Button
 
-        # ---------------------------------------------------------------------------------------------- Input Functions Button
-
-        # Drag and Drop Functions ---------------------------------------------------------------------------------------------
+        # Drag and Drop Functions -------------------------------------------------------------------------------------
         def video_drop_input(event):  # Drag and drop function
             input_dnd.set(event.data)
 
-
-        def update_file_input(*args):  # Drag and drop block of code
-            global chapter_source_input, autofilesave_dir_path, VideoInputQuoted, output, autosavefilename, output_quoted, extension_type
+        def update_file_input(*args):  # Drag and drop block of code, virtually the same as the above function
+            global chapter_source_input, autofilesave_dir_path, VideoInputQuoted, output, autosavefilename, \
+                output_quoted, extension_type
             chap_input_entry.configure(state=NORMAL)
             chap_input_entry.delete(0, END)
-            chapter_source_input = str(input_dnd.get()).replace("{", "").replace("}", "")
+            chapter_source_input = str(input_dnd.get()).replace("{", "").replace("}", "")  # Remove extra from dnd file
             media_info = MediaInfo.parse(pathlib.Path(chapter_source_input))
             for track in media_info.tracks:
                 if track.track_type == 'General':
@@ -156,17 +164,18 @@ def launch_chapter_demuxer():
                 else:
                     messagebox.showinfo(title='Input Not Supported', message='Try again with a supported file!\n\n' +
                                                                              'Unsupported file extension "' +
-                                                                             str(pathlib.Path(chapter_source_input).suffix) +
-                                                                             '"')
+                                                                             str(pathlib.Path(
+                                                                                 chapter_source_input).suffix) + '"')
                     extract_button.configure(state=DISABLED)
 
+        # ------------------------------------------------------------------------------------- Drag and Drop Functions
 
-        # --------------------------------------------------------------------------------------------- Drag and Drop Functions
-
+        # Buttons, Entry's and Commands -------------------------------------------------------------------------------
         input_dnd = StringVar()
         input_dnd.trace('w', update_file_input)
-        chap_input_button = HoverButton(chapter_extract, text='Input', command=input_button_commands, foreground='white',
-                                        background='#23272A', borderwidth='3', activebackground='grey', width=15)
+        chap_input_button = HoverButton(chapter_extract, text='Input', command=input_button_commands,
+                                        foreground='white', background='#23272A', borderwidth='3',
+                                        activebackground='grey', width=15)
         chap_input_button.grid(row=0, column=0, columnspan=1, padx=(10, 5), pady=5, sticky=W + E)
         chap_input_button.drop_target_register(DND_FILES)
         chap_input_button.dnd_bind('<<Drop>>', video_drop_input)
@@ -176,7 +185,7 @@ def launch_chapter_demuxer():
         chap_input_entry.drop_target_register(DND_FILES)
         chap_input_entry.dnd_bind('<<Drop>>', video_drop_input)
 
-
+        # Simple output button commands -------------------------------------------------------------------------------
         def output_button_commands():
             global output, output_quoted
             output_window = filedialog.asksaveasfilename(defaultextension=".txt", initialdir=autofilesave_dir_path,
@@ -191,40 +200,51 @@ def launch_chapter_demuxer():
                 chap_output_entry.insert(0, output)
                 chap_output_entry.configure(state=DISABLED)
 
-
-        chap_output_button = HoverButton(chapter_extract, text='Output', command=output_button_commands, foreground='white',
-                                         background='#23272A', borderwidth='3', activebackground='grey', width=15)
+        chap_output_button = HoverButton(chapter_extract, text='Output', command=output_button_commands,
+                                         foreground='white', background='#23272A', borderwidth='3',
+                                         activebackground='grey', width=15)
         chap_output_button.grid(row=1, column=0, columnspan=1, padx=(10, 5), pady=(40, 5), sticky=W + E)
         chap_output_entry = Entry(chapter_extract, borderwidth=4, background='#CACACA', state=DISABLED, width=30)
         chap_output_entry.grid(row=1, column=1, columnspan=2, padx=(5, 10), pady=(40, 5), sticky=W + E)
 
+        # ------------------------------------------------------------------------------- Simple output button commands
 
+        # Start Job code ----------------------------------------------------------------------------------------------
         def start_job():
             global output
             output_quoted = f'"{output}"'
 
-            if extension_type == '.mp4':
-                finalcommand = '"' + mp4box + ' ' + f'"{chapter_source_input}"' + ' -dump-chap-ogg -out ' + output_quoted + '"'
-            elif extension_type == '.mkv':
-                finalcommand = '"' + mkvextract + ' ' + f'"{chapter_source_input}"' + ' ' + 'chapters -s ' + output_quoted + '"'
+            if extension_type == '.mp4':  # If extension type is mp4 run this command
+                finalcommand = '"' + mp4box + ' ' + f'"{chapter_source_input}"' + ' -dump-chap-ogg -out ' \
+                               + output_quoted + '"'
+            elif extension_type == '.mkv':  # If extension type is mkv run this command
+                finalcommand = '"' + mkvextract + ' ' + f'"{chapter_source_input}"' + ' ' + 'chapters -s ' \
+                               + output_quoted + '"'
 
+            # Use subprocess.check_output to execute command then wait to finish executing before code moves to next
             subprocess.check_output('cmd /c ' + finalcommand, universal_newlines=True,
                                     creationflags=subprocess.CREATE_NO_WINDOW)
 
-            if pathlib.Path(output).is_file():
+            if pathlib.Path(output).is_file():  # Once job is completed, update status label to say 'Completed'
                 status_label.configure(text='Completed!')
 
+        # ---------------------------------------------------------------------------------------------- Start Job code
 
         extract_button = HoverButton(chap_extract_win, text='Extract', command=start_job, foreground='white',
-                                     background='#23272A', borderwidth='3', activebackground='grey', width=15, state=DISABLED)
+                                     background='#23272A', borderwidth='3', activebackground='grey', width=15,
+                                     state=DISABLED)
         extract_button.grid(row=2, column=2, columnspan=1, padx=(20, 20), pady=(20, 5), sticky=W + E)
 
         status_label = Label(chap_extract_win, text='Select "Input" or drag and drop a MKV or MP4 file to begin...',
                              bd=4, relief=SUNKEN, anchor=E, background='#717171', foreground="white")
         status_label.grid(column=0, row=3, columnspan=4, sticky=W + E, pady=(0, 2), padx=5)
 
+        # ------------------------------------------------------------------------------- Buttons, Entry's and Commands
+
+        # End of mainloop ---------------------------------------------------------------------------------------------
         chap_extract_win.mainloop()
+        # --------------------------------------------------------------------------------------------- End of mainloop
 
 
-if standalone:
+if standalone:  # If standalone is set to True, then automatically start the above code
     launch_chapter_demuxer()
